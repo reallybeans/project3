fab599ab2ea4a5fcb3912ccfb3d5c4c4060c42b1
Bean <vr_anh.lenhut@capitaland.com>
2024-10-22 15:58:03 +0700

    change confugration

--- analytics/config.py
+++ analytics/config.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 db_username = os.environ["DB_USERNAME"]
 db_password = os.environ["DB_PASSWORD"]
 db_host = os.environ.get("DB_HOST", "127.0.0.1")
-db_port = os.environ.get("DB_PORT", "5432")
+db_port = os.environ.get("DB_PORT", "5433")
 db_name = os.environ.get("DB_NAME", "postgres")
 
 app = Flask(__name__)
